dddcb98108bbd953d36db7039b2a62ec130e163b
Vivek Kumar <vivek.kumar02@nagarro.com>
2019-02-02 18:17:40 +0530

    First Commit

---
+++ src/main/java/com/vivek/BHB_App/BhbAppApplication.java
@@ -0,0 +1,14 @@
+package com.vivek.BHB_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhbAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vivek/BHB_App/BhbAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vivek.BHB_App;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BhbAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
