b4419a6438142f391a25bee2fd7b1054900ea7d1
Vivek Seth <vivekseth09@gmail.com>
2019-02-19 23:04:07 +0530

    Initial commit

--- src/main/java/com/vivek/BHB_App/BhbAppApplication.java
+++ src/main/java/com/vivek/BHB_App/BhbAppApplication.java
@@ -2,8 +2,10 @@ package com.vivek.BHB_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.vivek.controller"} )
 public class BhbAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/vivek/controller/WelcomeController.java
+++ src/main/java/com/vivek/controller/WelcomeController.java
@@ -1,9 +1,9 @@
 package com.vivek.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/myApp")
 public class WelcomeController {
 	
